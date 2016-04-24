27be573b1abf25f0813d25c5ca5144d63c01693b
Valentin Volkl <valentin.volkl@cern.ch>
2016-04-24 14:01:30 +0200

    Add matplotlib setup and imports.

--- matplotlib_plugin.py
+++ matplotlib_plugin.py
@@ -1,4 +1,10 @@
 
 """ Draw GDML volumes with matplotlib. """
 
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
