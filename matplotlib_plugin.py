6bbff1d388393e698db6e5a1a00a50eacf01917b
Valentin Volkl <valentin.volkl@cern.ch>
2016-04-25 20:23:47 +0200

    inf up

--- matplotlib_plugin.py
+++ matplotlib_plugin.py
@@ -8,3 +8,13 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+def plot_box(box, ax=None,):
+    vertices = [[0, 1, 2,3], [3,4,5],[0,3,4,1]]
+    tupleList = zip(x, y, z)
+    print tupleList
+
+
+
+    poly3d = [[tupleList[vertices[ix][iy]] for iy in range(len(vertices[ix]))] for ix in range(len(vertices))]
+
+
